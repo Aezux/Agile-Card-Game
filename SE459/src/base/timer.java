ac240f3b895f6dc7dfe610a1ac16c838a534c4ad
nvemich <nemich@mail.depaul.com>
2018-10-12 14:57:37 -0500

    Countdown added

--- SE459/src/base/timer.java
+++ SE459/src/base/timer.java
@@ -1,7 +1,49 @@
 package base;
 
-public class timer {
+import java.awt.Toolkit;
+import java.util.Timer;
+import java.util.TimerTask;
 
-	//create timer
-		
+public class timer {
+  Toolkit toolkit;
+  Timer timer;
+int t=10000,total;
+//  public timer(int seconds) {
+//    toolkit = Toolkit.getDefaultToolkit();
+//    timer = new Timer();
+//    total =seconds * t;
+//    System.out.println(total);
+////    timer.schedule(new RemindTask(), total); 
+//  }
+
+
+public timer(int seconds) {
+    timer = new Timer();
+    timer.scheduleAtFixedRate(new RemindTask(seconds), 0, 1000);
+}
+
+class RemindTask extends TimerTask {
+    private volatile int remainingTimeInSeconds;
+    public RemindTask(int remainingTimeInSeconds) {
+        this.remainingTimeInSeconds = remainingTimeInSeconds;
+    }
+
+    public void run() {
+        if (remainingTimeInSeconds != 0) {
+            System.out.println(remainingTimeInSeconds + " ...");
+            remainingTimeInSeconds -= 1;
+        }
+    }
+}
+
+public static void main(String args[]) throws InterruptedException {
+    timer t = new timer(5);
+    System.out.println("Timer started");
+    Thread.sleep(5000);
+    t.end();
+}
+
+private void end() {
+    this.timer.cancel();
+}
 }
