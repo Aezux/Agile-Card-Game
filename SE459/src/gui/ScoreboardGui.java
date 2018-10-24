c5a136bc3fc83e4e54e91677af3a7dcb80e767af
nvemich <nemich@mail.depaul.com>
2018-10-24 14:53:35 -0500

    Set up base GUI for the scores.

--- SE459/src/gui/ScoreboardGui.java
+++ SE459/src/gui/ScoreboardGui.java
@@ -1,6 +1,65 @@
 package gui;
 
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class ScoreboardGui {
-	//will be used to show the scoreboard
-	//will need to use correct answers and scores from teams classes
-}
+
+
+	    public static void main(String[] args) {
+	        new ScoreboardGui();
+	    }
+
+	    public ScoreboardGui() {
+	        EventQueue.invokeLater(new Runnable() {
+	            @Override
+	            public void run() {
+	                try {
+	                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+	                    ex.printStackTrace();
+	                }
+
+	                JFrame frame = new JFrame("Score Board");
+	                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	                frame.add(new ScorePane());
+	                frame.pack();
+	                frame.setLocationRelativeTo(null);
+	                frame.setVisible(true);
+	            }
+	        });
+	    }
+
+	    public class ScorePane extends JPanel {
+
+	        private JLabel label;
+	        //private Team teamSet = new Team();
+
+
+
+	        public ScorePane() {
+	            setLayout(new GridBagLayout());
+	            GridBagConstraints gbc = new GridBagConstraints();
+	            gbc.insets = new Insets(5, 5, 5, 5);
+	            gbc.gridwidth = GridBagConstraints.REMAINDER;
+
+	            label = new JLabel("Scores");
+	            add(label, gbc);
+
+
+	        }
+
+	    }
+
+	}
