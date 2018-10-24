2b25249c2d0524720e1831d852b36cea1ab3d7ad
anPerez4120 <anthony_p4991@yahoo.com>
2018-10-24 15:26:09 -0500

    Removed the text from the cards, and also made them draggable

--- src/game/Main.java
+++ src/game/Main.java
@@ -2,6 +2,7 @@ package game;
 
 import java.util.ArrayList;
 
+import game.choice.ImageFinder;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -9,6 +10,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Point2D;
 //import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -32,17 +34,31 @@ public class Main extends Application{
 	Point2D clickPoint;
 	Stage window;
 	
+	final int dimensions = 20;
+	final int scale = 50;
+	
+	ShapeComponent currentComponent;
+	boolean inDragMode;
+	Point2D lastPosition;
+	double initialPositionX = 0;
+	double initialPositionY = 0;
+	
 	public static void main(String args[]) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+
 //		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 //		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		Pane root = new AnchorPane();
-		Scene scene = new Scene(root, 1050, 900);
+		Scene scene = new Scene(root, 1300, 900);
+		
+//		Adds the ImageView before anything else 
+		ImageFinder image = new ImageFinder();
+		ImageView imageView = new ImageView(image.getImage("background.jpg", scale, dimensions+7));
+		root.getChildren().add(imageView);
 		
 		
 		
@@ -59,34 +75,37 @@ public class Main extends Application{
 		int y = 200;
 		Question question = new Question();
 		HBox hand = new HBox();
-		for (int i = 0; i < 6; i ++) {				
+		for (int i = 0; i < 6; i++) {				
 			Card card = question.getCard("27");
 			System.out.println("Question: " + card.getQuestion());
 			System.out.println("Choices: " + card.getChoices());
 			System.out.println("Answer: " + card.getAnswer());
 			System.out.println("Points: " + card.getPoints());
 			System.out.println("Category:" + card.getCategory());
-			StackPane layout = new StackPane();
-			Text text = new Text(card.getQuestion());
-			text.setLayoutX(200);
-			int width = (int) text.getLayoutBounds().getWidth();
-			System.out.println("Width" + width);
-			CardComponent cardComponent = new CardComponent(new Point2D(x, y), 100, width, card);		
 			
-			layout.getChildren().addAll(cardComponent.getRect(), text);
-			layout.setLayoutX(x);
-			layout.setLayoutY(y);
-			layout.setPadding(new Insets (10));
-			hand.getChildren().add(layout);
+//			StackPane layout = new StackPane();
+//			Text text = new Text(card.getQuestion());
+//			int width = (int) text.getLayoutBounds().getWidth();
+//			System.out.println("Width" + width);
+//			
+//			Making the cardComponent based on the size of the text (width+10 is used for padding)
+//			CardComponent cardComponent = new CardComponent(new Point2D(x, y), 100, width+10, card);		
+//			
+//			layout.getChildren().addAll(cardComponent.getRect(), text);
+//			layout.setLayoutX(x);
+//			layout.setLayoutY(y);
+//			layout.setPadding(new Insets (10));
+//			hand.getChildren().add(layout);
 			
-//			root.getChildren().add(layout);
+			CardComponent cardComponent = new CardComponent(new Point2D(x, y), 100, 150, card);
+			root.getChildren().add(cardComponent.getRect());
 			handDealt.add(cardComponent);
-			x += 300;
+			x += 200;
 		}
 		
-		hand.setLayoutX(20);
-		hand.setLayoutY(100);
-		root.getChildren().add(hand);
+//		hand.setLayoutX(20);
+//		hand.setLayoutY(100);
+//		root.getChildren().add(hand);
 		window = primaryStage;
 		window.setTitle("Agile Scrum Master");
 		window.setScene(scene);
@@ -97,24 +116,67 @@ public class Main extends Application{
 
 		@Override
 		public void handle(MouseEvent mouseEvent) {
-			// TODO Auto-generated method stub
 			
 			clickPoint = new Point2D(mouseEvent.getX(), mouseEvent.getY());
 			String eventName = mouseEvent.getEventType().getName();
 			
+			
+			if (!inDragMode) {
+				currentComponent = getCurrentComponent();
+			}
+			
 			switch(eventName) {
 			
 				case("MOUSE_RELEASED"):
-					
+					if (currentComponent != null && currentComponent instanceof CardComponent) {
+//						Still need to implement code here for when they release the card on a player 
+						
+//						Puts the card back to where it was when it was initially created
+						((CardComponent)currentComponent).getRect().setX(initialPositionX);
+						((CardComponent)currentComponent).getRect().setY(initialPositionY);
+					}
+					else {
+						currentComponent = null;
+					}
+					inDragMode = false;
 					break;
 				case("MOUSE_DRAGGED"):
+					inDragMode = true;
+					if (currentComponent != null && lastPosition != null) {
+						
+						((CardComponent) currentComponent).getRect().getX();
+						((CardComponent) currentComponent).getRect().getY();
 					
+//						Moves the card based on where the mouse is dragged
+						double delataX = clickPoint.getX()-lastPosition.getX();
+						double delataY = clickPoint.getY()-lastPosition.getY();
+						currentComponent.move(delataX, delataY);
+						
+					}
 					break;
 				case("MOUSE_PRESSED"):
-					
+					if (currentComponent != null) {
+						initialPositionX = ((CardComponent) currentComponent).getRect().getX();
+						initialPositionY = ((CardComponent) currentComponent).getRect().getY();
+						System.out.println("X: " + initialPositionX);
+						System.out.println("Y: " + initialPositionY);
+					}
 					break;
 			}
+			lastPosition = clickPoint;
 		}
 		
 	};
+	
+//	Looks to see what component is at the clickPoint, if none return null
+	private CardComponent getCurrentComponent() {
+		CardComponent currentComponent = null;
+		for (CardComponent comp : handDealt) {
+			if (comp.contains(clickPoint)) {
+				currentComponent = comp;
+			}
+		}
+		return currentComponent;
+	}
+	
 }
