462aead850bb7552e66b79549e0faae613cf97fe
nvemich <nemich@mail.depaul.com>
2018-11-08 11:05:48 -0600

    Added the scores from the singleton class. Debating moving to javaFX

--- SE459/src/gui/ScoreboardGui.java
+++ SE459/src/gui/ScoreboardGui.java
@@ -1,18 +1,18 @@
 package gui;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+
+
+
+import base.PointsKeeperSingleton;
 
 public class ScoreboardGui {
 
@@ -25,11 +25,11 @@ public class ScoreboardGui {
 	        EventQueue.invokeLater(new Runnable() {
 	            @Override
 	            public void run() {
-	                try {
-	                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-	                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-	                    ex.printStackTrace();
-	                }
+//	                try {
+//	                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//	                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+//	                    ex.printStackTrace();
+//	                }
 
 	                JFrame frame = new JFrame("Score Board");
 	                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -41,11 +41,13 @@ public class ScoreboardGui {
 	        });
 	    }
 
-	    public class ScorePane extends JPanel {
+	    @SuppressWarnings("serial")
+		public class ScorePane extends JPanel {
 
 	        private JLabel label;
 	        //private Team teamSet = new Team();
-
+	        int team1 = PointsKeeperSingleton.getUniqueInstance().getTeamScore("1");
+	        int team2 = PointsKeeperSingleton.getUniqueInstance().getTeamScore("2");
 
 
 	        public ScorePane() {
@@ -56,8 +58,46 @@ public class ScoreboardGui {
 
 	            label = new JLabel("Scores");
 	            add(label, gbc);
+	            
+	            label = new JLabel("Team 1");
+	            label.setText("Team 1");
+	            add(label, gbc);
+	            
+	            
+	            label = new JLabel("Team 1 Scores");
+	            label.setText(Integer.toString(team1));
+	            add(label, gbc);
+	            
+	            
+	            label = new JLabel("Team 2");
+	            label.setText("Team 2");
+	            add(label, gbc);
+	            
+	            label = new JLabel("Team 2 Scores");
+	            label.setText(Integer.toString(team2));
+	            add(label, gbc);
+	            
 
 
+	            Font labelFont = label.getFont();
+	            String labelText = label.getText();
+
+	            int stringWidth = label.getFontMetrics(labelFont).stringWidth(labelText);
+	            int componentWidth = label.getWidth();
+
+	            // Find out how much the font can grow in width.
+	            double widthRatio = (double)componentWidth / (double)stringWidth;
+
+	            int newFontSize = (int)(labelFont.getSize() * widthRatio);
+	            int componentHeight = label.getHeight();
+
+	            // Pick a new font size so it will not be larger than the height of label.
+	            int fontSizeToUse = Math.min(newFontSize, componentHeight);
+
+	            // Set the label's font size to the newly determined size.
+	            label.setFont(new Font(labelFont.getName(), Font.PLAIN, fontSizeToUse));
+	            
+	            
 	        }
 
 	    }
